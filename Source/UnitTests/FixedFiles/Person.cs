e6429c429d2f907333c4ccc9c295c1d19dc74e90
brembos <stig@brembo.name>
2011-06-10 15:50:49 +0200

    major rewrite. layout interfaces and some cut-n-paste code

---
+++ Source/UnitTests/FixedFiles/Person.cs
@@ -0,0 +1,13 @@
+﻿using LinqToFlatFile;
+
+namespace UnitTests.FixedFiles
+{
+    public class Person
+    {
+        [FixedPosition(0, 4)]
+        public int Id { get; set; }
+
+        [FixedPosition(5, 24)]
+        public string Name { get; set; }
+    }
+}
---
+++ Source/UnitTests/FixedFiles/TabFileReaderTest.cs
@@ -0,0 +1,18 @@
+﻿using LinqToFlatFile;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.FixedFiles
+{
+    [TestClass()]
+    public class FixedPositionWriterTests
+    {
+        [TestMethod()]
+        public void WriteLineTest()
+        {
+            var person = new Person() {Id = 123, Name = "Elvis Presley"};
+            var writer = new LinqToFlatFile.FixedFileWriter<Person>();
+            var line = writer.MakeLine(person);
+            Assert.AreEqual("00123Elvis Presley       ",line);
+        }
+    }
+}
